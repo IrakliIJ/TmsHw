d8000afbbd88b64422ccd695ec8b80437d3b8806
IrakliIJ <tavartkiladze.it@gmail.com>
2023-02-18 19:17:09 +0400

    Inicial commit

---
+++ src/Ge/TeachmeSkils/HomeWorks/Hw_10022023/FirstHomeWorksClass.java
@@ -0,0 +1,10 @@
+package Ge.TeachmeSkils.HomeWorks.Hw_10022023;
+
+public class FirstHomeWorksClass {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("Sum :" + sum);
+    }
+}
