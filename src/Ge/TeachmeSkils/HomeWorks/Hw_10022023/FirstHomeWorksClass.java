1f60384671d39f54fc1ea2dac465e0d5d753e1ad
IrakliIJ <tavartkiladze.it@gmail.com>
2023-02-18 19:52:17 +0400

    Smal Changes

--- src/Ge/TeachmeSkils/HomeWorks/Hw_10022023/FirstHomeWorksClass.java
+++ src/Ge/TeachmeSkils/HomeWorks/Hw_10022023/FirstHomeWorksClass.java
@@ -2,7 +2,7 @@ package Ge.TeachmeSkils.HomeWorks.Hw_10022023;
 
 public class FirstHomeWorksClass {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 6;
         int b = 5;
         int sum = a + b;
         System.out.println("Sum :" + sum);
